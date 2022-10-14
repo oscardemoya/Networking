79fa856ecfe7f0add98c02685778b28e1631d35b
Oscar De Moya <oscar.demoya@koombea.com>
2022-10-13 22:54:02 -0500

    Create URLQuery collection using ExpressibleByDictionaryLiteral protocol

---
+++ Sources/Networking/URL/URLQueryItem.swift
@@ -0,0 +1,20 @@
+//
+//  URLQueryItem.swift
+//  
+//
+//  Created by Oscar De Moya on 13/10/22.
+//
+
+import Foundation
+
+extension Array<URLQueryItem>: ExpressibleByDictionaryLiteral {
+    public init(dictionaryLiteral elements: (String, String?)...) {
+        self = elements.map { URLQueryItem(name: $0.0, value: $0.1) }
+    }
+}
+
+extension Collection<URLQueryItem> {
+    subscript(_ name: String) -> String? {
+        first { $0.name == name }?.value
+    }
+}
